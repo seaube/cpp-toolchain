1e234e025004111a07bf0eb58b7272ea122d342a
Caleb Zulawski <czulawski@lgsinnovations.com>
2020-10-16 16:01:11 -0400

    Begin ergo implementation

--- docker-make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build --tag portable-llvm-env --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` docker
-docker run --rm -v `pwd`:/host -w /host portable-llvm-env make $@
