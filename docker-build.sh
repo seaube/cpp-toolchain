58bbade0b5fc66239a1c667e80fba0c6a203cc9e
Caleb Zulawski <czulawski@lgsinnovations.com>
2020-10-21 18:59:10 -0400

    Make implementation more modular

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-if [ ! -f docker/ergo.tar.gz ]; then
-    curl -L https://ws-apps.redacted.invalid/artifactory/BOB/ergo/ergo-1.0.0-beta.2.tar.gz -o docker/ergo.tar.gz
+if [ ! -f linux/docker/ergo.tar.gz ]; then
+    curl -L https://ws-apps.redacted.invalid/artifactory/BOB/ergo/ergo-1.0.0-beta.2.tar.gz -o linux/docker/ergo.tar.gz
 fi
-docker build --tag portable-llvm-env --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` docker
-docker run --rm --net=host -v `pwd`:/host -w /host portable-llvm-env ergo --log debug .
+docker build --tag portable-llvm-env --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` linux/docker
+docker run --rm --net=host -v `pwd`:/host -w /host portable-llvm-env ergo --log debug build
