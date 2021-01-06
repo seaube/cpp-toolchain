6280d7de0df183c7e12c888a801016fcb29e5bb9
Caleb Zulawski <czulawski@lgsinnovations.com>
2021-01-06 17:25:42 -0500

    Rename docker container

--- docker-build.sh
+++ docker-build.sh
@@ -3,5 +3,5 @@ set -e
 if [ ! -f linux/docker/ergo.tar.gz ]; then
     curl -L https://ws-apps.redacted.invalid/artifactory/BOB/ergo/ergo-1.0.0-beta.7-linux.tar.gz -o linux/docker/ergo.tar.gz
 fi
-docker build --tag portable-llvm-env --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` linux/docker
-docker run --rm --net=host -v `pwd`:/host -w /host portable-llvm-env ergo --log debug build
+docker build --tag wipal-toolchain-env --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` linux/docker
+docker run --rm --net=host -v `pwd`:/host -w /host wipal-toolchain-env ergo --log debug build
