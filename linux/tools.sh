775f359755b7cf81c8c270a9894f02e82c1bb7d1
Caleb Zulawski <czulawski@lgsinnovations.com>
2021-04-23 20:46:59 -0400

    Allow --target option to override default target.  Remove target-prefixed tools that are already universal.

--- linux/tools.sh
+++ linux/tools.sh
@@ -3,50 +3,58 @@
 BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TOOL="$(basename "$0")"
 
-# Determine the tool prefix
-case $TOOL in
-    x86_64-unknown-linux-gnu-*)
-        TARGET=x86_64-unknown-linux-gnu
-        PREFIX="${TARGET}-"
-        ;;
-    aarch64-unknown-linux-gnu-*)
-        TARGET=aarch64-unknown-linux-gnu
-        PREFIX="${TARGET}-"
-        ;;
-    *)
-        TARGET=$(cat ${BINDIR}/../libexec/wut/host)
-        PREFIX=""
-        ;;
-esac
-
-SYSROOT=$BINDIR/../libexec/wut/gcc/$TARGET/$TARGET/sysroot
-
 unsupported () {
     echo "Invalid flag for this toolchain ($1)" >&2
     exit 1
 }
 
+default_target() {
+    # Determine the tool prefix
+    case $TOOL in
+        x86_64-unknown-linux-gnu-*)
+            echo x86_64-unknown-linux-gnu
+            ;;
+        aarch64-unknown-linux-gnu-*)
+            echo aarch64-unknown-linux-gnu
+            ;;
+        *)
+            # Use the host if unspecified
+            cat ${BINDIR}/../libexec/wut/host
+            ;;
+    esac
+}
+
+sysroot() {
+    echo "$BINDIR/../libexec/wut/gcc/$1/$1/sysroot"
+}
+
 compiler_args() {
     # set language-specific flags
     case $1 in
-        c)  LINK_FLAGS="-fuse-ld=lld -static-libgcc -static-libgcc" ;;
+        c)  LINK_FLAGS="-fuse-ld=lld -static-libgcc" ;;
         c++) LINK_FLAGS="-fuse-ld=lld -static-libstdc++ -static-libgcc" ;;
     esac
     shift 1
 
-    COMPILER_FLAGS="--target=$TARGET --sysroot=$SYSROOT -gcc-toolchain $BINDIR/../libexec/wut/gcc/$TARGET"
+    TARGET=$(default_target)
 
-    # Check for incompatible flags
-    for ARG in "$@"; do
-        case $ARG in
+    # Handle flags
+    FLAGS=""
+    while(($#)) ; do
+        case $1 in
             -c|-S)
                 # We aren't linking, so don't use any link flags
                 LINK_FLAGS=""
+                FLAGS="$FLAGS $1"
+                ;;
+            --target=*)
+                TARGET=${1#--target=}
+                ;;
+            -target)
+                TARGET=$2
+                shift 1
                 ;;
             -fuse-ld*)
-                unsupported $ARG
-                ;;
-            --target|--target=*)
                 unsupported $ARG
                 ;;
             --sysroot|--sysroot=*)
@@ -56,10 +64,13 @@ compiler_args() {
                 unsupported $ARG
                 ;;
             *)
+                FLAGS="$FLAGS $1"
                 ;;
         esac
+        shift 1
     done
-    echo "$COMPILER_FLAGS $LINK_FLAGS $@"
+
+    echo "--target=$TARGET --sysroot=$(sysroot $TARGET) -gcc-toolchain $BINDIR/../libexec/wut/gcc/$TARGET $LINK_FLAGS $FLAGS"
 }
 
 linker_args() {
@@ -73,50 +84,51 @@ linker_args() {
                 ;;
         esac
     done
-    echo "--sysroot=$SYSROOT $@"
+    TARGET=$(default_target)
+    echo "--sysroot=$(sysroot $TARGET) $@"
 }
 
 case $TOOL in
-    ${PREFIX}c++|${PREFIX}clang++)
+    c++|*-c++|*-clang++)
         $BINDIR/../libexec/wut/llvm/bin/clang++ $(compiler_args c++ $@)
         ;;
-    ${PREFIX}cc|${PREFIX}clang)
+    cc|*-cc|*-clang)
         $BINDIR/../libexec/wut/llvm/bin/clang $(compiler_args c $@)
         ;;
-    ${PREFIX}ld)
+    ld|*-ld)
         $BINDIR/../libexec/wut/llvm/bin/ld.lld $(linker_args $@)
         ;;
-    ${PREFIX}ar)
+    ar)
         $BINDIR/../libexec/wut/llvm/bin/llvm-ar $@
         ;;
-    ${PREFIX}ranlib)
+    ranlib)
         $BINDIR/../libexec/wut/llvm/bin/llvm-ar s $@
         ;;
-    ${PREFIX}strip)
+    strip)
         $BINDIR/../libexec/wut/llvm/bin/llvm-strip $@
         ;;
-    ${PREFIX}nm)
+    nm)
         $BINDIR/../libexec/wut/llvm/bin/llvm-nm $@
         ;;
-    ${PREFIX}objcopy)
+    objcopy)
         $BINDIR/../libexec/wut/llvm/bin/llvm-objcopy $@
         ;;
-    ${PREFIX}objdump)
+    objdump)
         $BINDIR/../libexec/wut/llvm/bin/llvm-objdump $@
         ;;
-    ${PREFIX}c++filt)
+    c++filt)
         $BINDIR/../libexec/wut/llvm/bin/llvm-cxxfilt $@
         ;;
-    ${PREFIX}addr2line)
+    addr2line)
         $BINDIR/../libexec/wut/llvm/bin/llvm-addr2line $@
         ;;
-    ${PREFIX}strings)
+    strings)
         $BINDIR/../libexec/wut/llvm/bin/llvm-strings $@
         ;;
-    ${PREFIX}readelf)
+    readelf)
         $BINDIR/../libexec/wut/llvm/bin/llvm-readelf $@
         ;;
-    ${PREFIX}size)
+    size)
         $BINDIR/../libexec/wut/llvm/bin/llvm-readelf $@
         ;;
     *)
