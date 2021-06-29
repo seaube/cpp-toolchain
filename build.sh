9245fc1383327d871399af96d7920d4162a1322c
Caleb Zulawski <caleb.zulawski@caci.com>
2021-06-29 16:13:00 -0400

    Add CI/CD build

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+case `uname` in
+    Linux)
+        if [ ! -f linux/docker/ergo.tar.gz ]; then
+            curl -L https://ws-apps.redacted.invalid/artifactory/BOB/ergo/ergo-1.0.0-beta.9-linux.tar.gz -o linux/docker/ergo.tar.gz
+        fi
+        docker build --tag wipal-toolchain-env --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` linux/docker
+        docker run --rm --net=host -v `pwd`:/host -w /host wipal-toolchain-env ergo --log debug build
+    ;;
+    Darwin)
+        if [ ! -f macos/ergo/ergo.tar.gz ]; then
+            mkdir -p macos/ergo
+            curl -L https://ws-apps.redacted.invalid/artifactory/BOB/ergo/ergo-1.0.0-beta.9-macos.tar.gz -o macos/ergo/ergo.tar.gz
+            tar -xf macos/ergo/ergo.tar.gz -C macos/ergo --strip-components 1
+        fi
+        macos/ergo/bin/ergo --log debug build
+    ;;
+esac
--- docker-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-if [ ! -f linux/docker/ergo.tar.gz ]; then
-    curl -L https://ws-apps.redacted.invalid/artifactory/BOB/ergo/ergo-1.0.0-beta.9-linux.tar.gz -o linux/docker/ergo.tar.gz
-fi
-docker build --tag wipal-toolchain-env --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` linux/docker
-docker run --rm --net=host -v `pwd`:/host -w /host wipal-toolchain-env ergo --log debug build
