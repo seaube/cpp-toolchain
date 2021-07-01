c671499cb64e3a21033dc288356418b35a5c426b
Caleb Zulawski <caleb.zulawski@caci.com>
2021-07-01 11:12:00 -0400

    Try curl docker image

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 case `uname` in
     Linux)
         if [ ! -f linux/docker/ergo.tar.gz ]; then
-            wget https://ws-apps.redacted.invalid/artifactory/BOB/ergo/ergo-1.0.0-beta.9-linux.tar.gz -O linux/docker/ergo.tar.gz
+            curl -L https://ws-apps.redacted.invalid/artifactory/BOB/ergo/ergo-1.0.0-beta.9-linux.tar.gz -o linux/docker/ergo.tar.gz
         fi
         docker build --tag wipal-toolchain-env --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` linux/docker
         docker run --rm --net=host -v `pwd`:/host -w /host wipal-toolchain-env ergo --log debug build
