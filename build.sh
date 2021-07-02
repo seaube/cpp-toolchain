a5fdeb85f7047e161be40193a3d610b61464bdc9
Caleb Zulawski <caleb.zulawski@caci.com>
2021-07-02 16:45:12 -0400

    Update version and fix build

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ case `uname` in
         if [ ! -f linux/docker/ergo.tar.gz ]; then
             curl -L https://ws-apps.redacted.invalid/artifactory/BOB/ergo/ergo-1.0.0-beta.9-linux.tar.gz -o linux/docker/ergo.tar.gz
         fi
+        cp ~/.config/wipal-artifactory-key linux/docker
         docker build --tag wipal-toolchain-env --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` linux/docker
         docker run --rm --net=host -v `pwd`:/host -w /host wipal-toolchain-env ergo --log debug build
     ;;
