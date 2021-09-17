8fad3c068604d915b898842177dc67fb90276a15
Caleb Zulawski <caleb.zulawski@caci.com>
2021-09-17 14:53:57 -0400

    Add missing dependency

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,4 +3,4 @@
 TZ=America/New_York
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt-get update
-apt-get install -y curl wget xz-utils git autoconf automake bison flex texinfo help2man gawk libtool-bin ncurses-dev python3-dev rsync make bzip2 unzip g++ diffutils cmake zlib1g-dev
+apt-get install -y ca-certificates curl wget xz-utils git autoconf automake bison flex texinfo help2man gawk libtool-bin ncurses-dev python3-dev rsync make bzip2 unzip g++ diffutils cmake zlib1g-dev
