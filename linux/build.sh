b848c08b549d950003f895f97c79a6d876e1d18b
Caleb Zulawski <caleb.zulawski@caci.com>
2024-09-17 10:01:31 -0400

    Use local make to avoid make 4.4 issue with old glibc

--- linux/build.sh
+++ linux/build.sh
@@ -7,11 +7,6 @@ target=$1
 config=$2
 ct_ng=$PWD/$3
 
-#cp $config $builddir/.config
-#mkdir $builddir/tarballs
-#(cd $builddir && $ct_ng build.1)
-#mv $builddir $target
-
 mkdir $target
 cp $config $target/.config
 mkdir $target/tarballs
