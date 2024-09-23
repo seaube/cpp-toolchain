5cfd63258ef608d5c30a20b5ccb4cfbd95a2f38a
Caleb Zulawski <caleb.zulawski@caci.com>
2024-09-23 10:58:47 -0400

    Get build working on Linux

--- docker.sh
+++ docker.sh
@@ -8,11 +8,24 @@ else
     DOCKER=podman
 fi
 
-$DOCKER build --tag wipal-toolchain-env docker
+if command -v dzdo &> /dev/null
+then
+    DOCKER="dzdo $DOCKER"
+fi
+
+BUILD_ROOT=`pwd`/.docker_cache
+mkdir -p $BUILD_ROOT
+
+if [ -z ${SSL_CERT_FILE} ]; then
+    SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+fi
+
+$DOCKER build --build-arg CONFIG_UID=`id -u` --build-arg CONFIG_GID=`id -g` --tag wipal-toolchain-env docker
 $DOCKER run --rm -it -w `pwd` \
     -v `pwd`:`pwd` \
+    -v $BUILD_ROOT:$BUILD_ROOT \
     -v /etc/localtime:/etc/localtime:ro \
-    -v "$SSL_CERT_FILE":/usr/lib/ssl/cert.pem:copy \
-    --env BAZELISK_HOME=/Volumes/wut-build-volume/bazelisk \
+    -v "$SSL_CERT_FILE":/usr/lib/ssl/cert.pem:ro \
+    --env BAZELISK_HOME=$BUILD_ROOT/bazelisk \
     wipal-toolchain-env \
-    bazelisk --output_base=/Volumes/wut-build-volume/output_base "$@"
+    bazelisk --output_base=$BUILD_ROOT/output_base "$@"
--- linux/build.sh
+++ linux/build.sh
@@ -2,12 +2,10 @@
 
 set -euo pipefail
 
-builddir=$(mktemp -d)
-target=$1
+builddir=$1/..
 config=$2
 ct_ng=$PWD/$3
 
-mkdir $target
-cp $config $target/.config
-mkdir $target/tarballs
-(cd $target && $ct_ng build.16)
+cp $config $builddir/.config
+mkdir $builddir/tarballs
+(cd $builddir && $ct_ng build.16)
