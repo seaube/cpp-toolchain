99d2c089a541fb481cda4afe5933b4065594e054
Caleb Zulawski <caleb.zulawski@caci.com>
2025-03-04 12:10:04 -0500

    Remove obsolete files

--- docker/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-apt-get update
-apt-get install -y curl wget xz-utils git autoconf automake bison flex texinfo help2man gawk libtool-bin ncurses-dev python3-dev rsync make bzip2 unzip g++ diffutils cmake zlib1g-dev libarchive-tools rsync
