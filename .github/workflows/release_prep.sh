230b70eadee58a0b869323db8c961efdff1f4990
Caleb Zulawski <caleb.zulawski@caci.com>
2025-04-09 13:55:25 -0400

    Add release workflow

---
+++ .github/workflows/release_prep.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+TAG=$1
+PREFIX="portable_cc_toolchain-${TAG:1}"
+ARCHIVE="bazel_portable_cc_toolchain-$TAG.tar.gz"
+git archive --format=tar --prefix=${PREFIX}/ ${TAG}:bazel --output=${ARCHIVE}
