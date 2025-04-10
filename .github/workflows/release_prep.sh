273c65166bcbb28d04acd9a8c381c52248700d68
Caleb Zulawski <caleb.zulawski@caci.com>
2025-04-09 20:23:46 -0400

    Fix release archive format

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -5,4 +5,4 @@ set -o errexit -o nounset -o pipefail
 TAG=$1
 PREFIX="portable_cc_toolchain-${TAG:1}"
 ARCHIVE="bazel_portable_cc_toolchain-$TAG.tar.gz"
-git archive --format=tar --prefix=${PREFIX}/ ${TAG}:bazel --output=${ARCHIVE}
+git archive --format=tar --prefix=${PREFIX}/ ${TAG}:bazel | gzip > ${ARCHIVE}
