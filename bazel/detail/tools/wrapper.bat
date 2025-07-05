ab481761cf1a2f9940294deede384758b739233f
Caleb Zulawski <caleb.zulawski@gmail.com>
2025-07-05 14:53:32 -0400

    Use a proper batch wrapper

--- bazel/detail/tools/wrapper.bat
+++ bazel/detail/tools/wrapper.bat
@@ -1,2 +1,22 @@
 @echo off
-bash "%~dp0TOOL.sh" %*
+setlocal enabledelayedexpansion
+
+set "args="
+set "first=1"
+
+for %%i in (%*) do (
+    set "arg=%%~i"
+    set "arg=!arg:__BAZEL_EXECUTION_ROOT__=%CD%!"
+
+    if defined first (
+        set "args=!arg!"
+        set "first="
+    ) else (
+        set "args=!args! !arg!"
+    )
+)
+
+set "EXE=TOOL"
+set "EXE=!EXE:/=\!"
+"!EXE!" !args!
+exit /b !exitcode!
