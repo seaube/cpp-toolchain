65b5e509c85b8508aaab0b940199aa0816a17480
Caleb Zulawski <caleb.zulawski@caci.com>
2025-07-17 11:21:20 -0400

    Fix CMake asset override.

--- cmake/portable_cc_toolchain/private/download.cmake
+++ cmake/portable_cc_toolchain/private/download.cmake
@@ -22,9 +22,9 @@ endfunction()
 
 find_root_binary_dir(_root_binary_dir)
 
+
+
 function(download location asset variable)
-    set(_url "${_ASSET_URL_${asset}}")
-    set(_sha "${_ASSET_SHA256_${asset}}")
     set(_out "${_root_binary_dir}/_portable_cc_toolchain/${asset}")
 
     if(DEFINED ${variable})
@@ -34,21 +34,35 @@ function(download location asset variable)
     endif()
 
     set(DOWNLOAD_ARGS)
-	if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
+    if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
         set(DOWNLOAD_ARGS DOWNLOAD_EXTRACT_TIMESTAMP OFF)
-	endif()
+    endif()
 
     if(DEFINED _override)
-        execute_process(
-            COMMAND ${CMAKE_COMMAND} -E tar xvf "${_override}"
-            WORKING_DIRECTORY "${_out}"
-            OUTPUT_QUIET
-            RESULT_VARIABLE _status
-        )
-        if(NOT _status EQUAL 0)
-            message(FATAL_ERROR "Could not extract ${_override}")
+        message("Overriding ${asset} from ${variable}: ${_override}")
+        set(_stamp ${_out}/.stamp)
+        if(NOT EXISTS ${_stamp} AND ${_override} IS_NEWER_THAN ${_stamp})
+            file(MAKE_DIRECTORY ${_out})
+            execute_process(
+                COMMAND ${CMAKE_COMMAND} -E tar xvf "${_override}"
+                WORKING_DIRECTORY "${_out}"
+                OUTPUT_QUIET
+                RESULT_VARIABLE _status
+            )
+            if(NOT _status EQUAL 0)
+                message(FATAL_ERROR "Could not extract ${_override}")
+            endif()
+            execute_process(
+                COMMAND ${CMAKE_COMMAND} -E touch ${_stamp}
+                RESULT_VARIABLE _status
+            )
+            if(NOT _status EQUAL 0)
+                message(FATAL_ERROR "Could not stamp ${_stamp}")
+            endif()
         endif()
     else()
+        set(_url "${_ASSET_URL_${asset}}")
+        set(_sha "${_ASSET_SHA256_${asset}}")
         FetchContent_Declare(
             ${asset}
             URL "${_url}"
